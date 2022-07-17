714b9b23caa12846342ebff787589ab82b5c12b9
Syrup <goodgamersz665@gmail.com>
2022-07-18 00:23:30 +0700

    first commit

---
+++ src/routes/form.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  let name = "";
+  let users = [];
+  $: username = users
+    ? users.find((x) => x.name.toLowercase().includes(name.toLowercase()))
+    : undefined;
+
+  onMount(async () => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    users = data;
+  });
+
+  // let promise = search(name)
+  console.log(users);
+</script>
+
+<input type="search" bind:value={name} />
+<p>{username}</p>
